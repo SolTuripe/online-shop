9a1a4b3c3bbf6afce26bd307fe35a11856a992b2
Sol Turipe <solcis.bio@gmail.com>
2022-09-04 21:26:59 +0200

    firts commit

---
+++ src/main/kotlin/Product.kt
@@ -0,0 +1,3 @@
+
+
+class Product(var name: String, var img: String, var reference: String, var id: Int, var price: Double, var description: String)
---
+++ src/main/kotlin/ShoppingCart.kt
@@ -0,0 +1,8 @@
+class ShoppingCart {
+    var shoppingProducts: MutableList<Product> = ArrayList()
+    private var total = 0.0
+
+    fun addProduct(product: Product) {
+        shoppingProducts.add(product)
+    }
+}
