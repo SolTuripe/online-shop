f46df70016e97dcbf3fa8e50c572e884ca82581c
Sol Turipe <solcis.bio@gmail.com>
2022-09-08 13:01:54 +0200

    new class OnlineStore

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -1,11 +1,11 @@
-class Product(id: Int, name: String, img: String, reference: String, price: Double, attribute: String, description: String) {
+class Product(id: Int, img: String, name: String, attribute: String, description: String, price: Double, reference: String) {
 
     private var productId = id
-    private var productName = name
     private var productImg = img
-    private var productReference = reference
-    private var productPrice = price
+    private var productName = name
     private var productAttribute = attribute
     private var productDescription = description
+    private var productPrice = price
+    private var productReference = reference
 }
 
---
+++ src/main/kotlin/ProductsStock.kt
@@ -0,0 +1,21 @@
+class ProductsStock {
+    private var productsStock: MutableList<Product> = mutableListOf(Product(
+        1,
+        "\uD83D\uDCFA",
+        "Television",
+        "Glamorous panoramic television, 13 inches",
+        "With this panoramic television, your friday nights will be boring no mor",
+        300.99,
+        "W2C"
+    ),
+    Product(
+        2,
+        "\uD83C\uDFB9",
+        "piano",
+        "Untuned musical keyboard, 4 octaves",
+        "Tired of your noisy neighbouring? Play this untuned musical keyboard for two hours at home and your " +
+                "neighbour will be ready to move to a building far away from youu",
+        1003.00,
+    "X4A"
+    ))
+}
