ea7e5e18972a956cfea8e63d75b2008db78ea76c
Sol Turipe <solcis.bio@gmail.com>
2022-09-08 12:04:43 +0200

    new stock class

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -1,3 +1,11 @@
+class Product(id: Int, name: String, img: String, reference: String, price: Double, attribute: String, description: String) {
 
+    private var productId = id
+    private var productName = name
+    private var productImg = img
+    private var productReference = reference
+    private var productPrice = price
+    private var productAttribute = attribute
+    private var productDescription = description
+}
 
-class Product(var name: String, var img: String, var reference: String, var id: Int, var price: Double, var description: String)
--- src/main/kotlin/ShoppingCart.kt
+++ src/main/kotlin/ShoppingCart.kt
@@ -5,4 +5,5 @@ class ShoppingCart {
     fun addProduct(product: Product) {
         shoppingProducts.add(product)
     }
+
 }
